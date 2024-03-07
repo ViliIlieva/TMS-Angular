e1d5feda9886fb366e797a909c83d4c56892adfd
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2024-03-07 13:43:54 +0200

    Create api.service.ts

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Task } from 'src/app/types/task';
+import { Comment } from 'src/app/types/comment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  //TASKS
+  getTasks() {
+    const { apiUrl } = environment;
+    return this.http.get<Task[]>(`${apiUrl}/tasks`);
+  }
+
+  getTask(id: string) {
+    const { apiUrl } = environment;
+    return this.http.get<Task>(`${apiUrl}/tasks/${id}`);
+  }
+
+  createTask(
+    taskName: string,
+    taskText: string,
+    status: string,
+    taskType: string,
+  ) {
+    const { apiUrl } = environment;
+    return this.http.post<Task>(`${apiUrl}/tasks`, {
+      taskName,
+      taskText,
+      status,
+      taskType,
+    });
+  }
+
+  //COMMENTS
+  getComments() {
+    const { apiUrl } = environment;
+    return this.http.get<Comment[]>(`${apiUrl}/comments`);
+  }
+}
