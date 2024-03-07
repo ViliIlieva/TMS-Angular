714dab60446f5c68ff57d162e1cd0ddfdf23e1e1
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2024-03-07 13:56:13 +0200

    Add files via upload

---
+++ frontend/src/app/task/new-task/new-task.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { UserService } from 'src/app/user/user.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.scss'],
+})
+export class NewTaskComponent {
+  constructor(
+    private apiService: ApiService,
+    private activatedRout: ActivatedRoute,
+    private userService: UserService,
+    private router: Router,
+  ) {}
+
+  addTask(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    const { taskName, taskText, taskType } = form.value;
+    const status = 'toDo';
+    
+    this.apiService
+      .createTask(taskName, taskText,status, taskType)
+      .subscribe(() => {
+        this.router.navigate(['/tasks']);
+      });
+  }
+}
