e62fadf82328d0077bed8b55b3406e6a0f38425b
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2024-03-07 14:00:44 +0200

    Add files via upload

---
+++ frontend/src/app/user-tasks/user-tasks.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-tasks',
+  templateUrl: './user-tasks.component.html',
+  styleUrls: ['./user-tasks.component.scss'],
+})
+export class UserTasksComponent {}
