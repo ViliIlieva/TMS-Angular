3754528733317469462de94222be44ee455ad49d
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2024-03-07 13:58:51 +0200

    Add files via upload

---
+++ frontend/src/app/user/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) {}
+
+  login(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+    const { username, password } = form.value;
+
+    this.userService.login(username, password).subscribe(() => {
+      this.router.navigate(['/tasks']);
+    });
+  }
+}
